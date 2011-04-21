a9e79859995f1e78ea15e0cbac07b6634b6deaa8
stes94 <stes94@ymail.com>
2011-04-21 19:23:37 +0000

    Added MouseMotionListener (Player)

--- src/Entities/Player.java
+++ src/Entities/Player.java
@@ -5,11 +5,15 @@ import java.util.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.lang.*;
 
-public class Player extends Character implements KeyListener
+public class Player extends Character implements KeyListener, MouseMotionListener
 {
 
+	private MouseEvent _lastMouseEvent;
+	
 	public Player()
 	{
 		super();
@@ -20,7 +24,10 @@ public class Player extends Character implements KeyListener
 	{
 		g.setColor(Color.BLACK);
 		g.fillRect((int)this.getLocation().getX()-5, (int)this.getLocation().getY()-5, 10, 10);
-		
+		if (_lastMouseEvent != null)
+		{
+			g.drawOval(_lastMouseEvent.getX()-3, _lastMouseEvent.getY()-3, 6, 6);
+		}
 	}
 
 	@Override
@@ -57,6 +64,18 @@ public class Player extends Character implements KeyListener
 				
 		}
 		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e)
+	{
+		_lastMouseEvent = e;		
 	}
 
 }
