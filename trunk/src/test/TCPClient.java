35e075f0fe576c9984293778eacfad0bbd1b961b
alex.belke@gmail.com <alex.belke@gmail.com@5dce331b-43ae-905c-cebe-8f68a0ec51fa>
2011-04-27 17:24:03 +0000

    udp and tcp test

---
+++ trunk/src/test/TCPClient.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.io.*;
+import java.net.*;
+
+class TCPClient
+{
+	public static void main(String argv[]) throws Exception
+	{
+		BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
+		Socket clientSocket = new Socket("localhost", 4185);
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		outToServer.writeBytes(inFromUser.readLine() + '\n');
+		String modifiedSentence = inFromServer.readLine();
+		System.out.println("FROM SERVER: " + modifiedSentence + '\n');
+		clientSocket.close();
+	}
+}
