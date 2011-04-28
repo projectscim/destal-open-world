5f0fbc0711364faa6a932a72047a7f49a20defd5
stes94@ymail.com <stes94@ymail.com@5dce331b-43ae-905c-cebe-8f68a0ec51fa>
2011-04-28 23:07:32 +0000

    Added I/O method in the Chunk class + specified data values for blocks in Values

--- trunk/src/entities/Tree.java
+++ trunk/src/entities/Tree.java
@@ -5,12 +5,18 @@ import java.util.*;
 import java.awt.*;
 import java.lang.*;
 
-public class Tree extends Block {
-
+public class Tree extends Block
+{
 	@Override
 	public void paint(Graphics g) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public int getDataValue()
+	{
+		return Values.TREE;
 	}
 
 }
