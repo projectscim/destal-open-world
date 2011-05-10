0a493a26e9623ef00f8842eb6de6f233032985a1
alex.belke@gmail.com <alex.belke@gmail.com@5dce331b-43ae-905c-cebe-8f68a0ec51fa>
2011-05-10 18:40:57 +0000

    basic serverside network stuff (TCP, multiple clients)

--- trunk/src/general/ClientConnection.java
+++ trunk/src/general/ClientConnection.java
@@ -1,10 +1,40 @@
 package general;
 
-import java.io.*;
-import java.util.*;
-import java.awt.*;
-import java.lang.*;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
-public class ClientConnection {
-
+public class ClientConnection implements Runnable
+{
+	private int _ID;
+	
+	private Socket _socket;
+	private ObjectInputStream _input;
+	private ObjectOutputStream _output;
+	
+	public ClientConnection(Socket s, int ID) throws Exception
+	{
+		_ID = ID;
+		_socket = s;
+		_input = new ObjectInputStream(_socket.getInputStream());
+		_output = new ObjectOutputStream(_socket.getOutputStream());
+	}
+	
+	public void run()
+	{
+		System.out.println("thread started (" + _ID + ")");
+		try
+        {
+			while(true) // TODO stop function
+			{
+				System.out.println(_input.readObject());
+				
+				_output.writeObject("Hi");
+				_output.flush();
+			}
+        }
+		catch(Exception e)
+		{
+		}
+	}
 }
