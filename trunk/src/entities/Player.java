323c702775a30b93c94c3f52a79055c9da2594b9
stes94@ymail.com <stes94@ymail.com@5dce331b-43ae-905c-cebe-8f68a0ec51fa>
2011-05-14 09:59:28 +0000

    Added repaint when player moves; Added move(boolean) method to Player

--- trunk/src/entities/Player.java
+++ trunk/src/entities/Player.java
@@ -1,5 +1,7 @@
 package entities;
 
+import general.GamePanel;
+
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
@@ -11,11 +13,23 @@ import java.awt.event.MouseMotionListener;
 public class Player extends Character implements KeyListener, MouseMotionListener
 {
 	private MouseEvent _lastMouseEvent;
+	private GamePanel _gamePanel;
 	
 	public Player()
 	{
 		super();
 		this.setLocation(20, 100);
+	}
+	
+	public Player(GamePanel container)
+	{
+		this();
+		_gamePanel = container;
+	}
+	
+	public void setContainer(GamePanel container)
+	{
+		_gamePanel = container;
 	}
 	
 	@Override
@@ -25,6 +39,22 @@ public class Player extends Character implements KeyListener, MouseMotionListene
 		g.fillRect((int)this.getLocation().getX()-5, (int)this.getLocation().getY()-5, 10, 10);
 	}
 
+	public void move(boolean forward)
+	{
+		double dx = (this.getLocation().distance(new Point(_lastMouseEvent.getX(), _lastMouseEvent.getY())) > 1 ? (_lastMouseEvent.getX()-this.getLocation().getX()) * 3 / this.getLocation().distance(new Point(_lastMouseEvent.getX(), _lastMouseEvent.getY())) : 0);
+		double dy = (this.getLocation().distance(new Point(_lastMouseEvent.getY(), _lastMouseEvent.getY())) > 1 ? (_lastMouseEvent.getY()-this.getLocation().getY()) * 3 / this.getLocation().distance(new Point(_lastMouseEvent.getX(), _lastMouseEvent.getY())) : 0);
+				
+		if (forward)
+		{
+			this.setLocation(this.getLocation().getX()+dx, this.getLocation().getY()+dy);
+		}
+		else
+		{
+			this.setLocation(this.getLocation().getX()-dx, this.getLocation().getY()-dy);
+		}
+		_gamePanel.invokeRepaint();
+	}
+	
 	@Override
 	public void keyPressed(KeyEvent e)
 	{
@@ -42,33 +72,7 @@ public class Player extends Character implements KeyListener, MouseMotionListene
 	@Override
 	public void keyTyped(KeyEvent e)
 	{
-		/*
-		double dx, dy;
-		if (this.getLocation().distance(new Point(_lastMouseEvent.getX(), _lastMouseEvent.getY())) != 0)
-		{
-			dx = (_lastMouseEvent.getX()-this.getLocation().getX()) * 3 / this.getLocation().distance(new Point(_lastMouseEvent.getX(), _lastMouseEvent.getY()));
-			dy = (_lastMouseEvent.getY()-this.getLocation().getY()) * 3 / this.getLocation().distance(new Point(_lastMouseEvent.getX(), _lastMouseEvent.getY()));
-		}
-		else
-		{
-			dx = 0;
-			dy = 0;
-		}*/
-		
-		// this is shorter, Alex'll be happy :D
-		double dx = (this.getLocation().distance(new Point(_lastMouseEvent.getX(), _lastMouseEvent.getY())) > 1 ? (_lastMouseEvent.getX()-this.getLocation().getX()) * 3 / this.getLocation().distance(new Point(_lastMouseEvent.getX(), _lastMouseEvent.getY())) : 0);
-		double dy = (this.getLocation().distance(new Point(_lastMouseEvent.getY(), _lastMouseEvent.getY())) > 1 ? (_lastMouseEvent.getY()-this.getLocation().getY()) * 3 / this.getLocation().distance(new Point(_lastMouseEvent.getX(), _lastMouseEvent.getY())) : 0);
-				
-		switch (e.getKeyChar())
-		{
-			case 'w':
-				this.setLocation(this.getLocation().getX()+dx, this.getLocation().getY()+dy);
-				break;
-			case 's':
-				this.setLocation(this.getLocation().getX()-dx, this.getLocation().getY()-dy);
-				break;
-				
-		}
+		this.move((e.getKeyChar() == 'w') ? true : false);
 	}
 	
 	@Override
@@ -80,7 +84,7 @@ public class Player extends Character implements KeyListener, MouseMotionListene
 	@Override
 	public void mouseMoved(MouseEvent e)
 	{
-		_lastMouseEvent = e;	
+		_lastMouseEvent = e;
 	}
 
 }
