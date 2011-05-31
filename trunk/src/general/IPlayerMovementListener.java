9332d02b9d135aaad728c6d9ce1ea9e94708d192
alex.belke@gmail.com <alex.belke@gmail.com@5dce331b-43ae-905c-cebe-8f68a0ec51fa>
2011-05-31 15:15:08 +0000

    added stes stuff

--- trunk/src/general/IPlayerMovementListener.java
+++ trunk/src/general/IPlayerMovementListener.java
@@ -1,6 +1,15 @@
 package general;
 
+/**
+ * 
+ * @author Steffen
+ *
+ */
 public interface IPlayerMovementListener
 {
+	/**
+	 * Invoked when a player moves
+	 * @param e
+	 */
 	public void playerMoved(PlayerMovementEvent e);
 }
