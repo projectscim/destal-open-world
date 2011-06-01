67b784acd9c550c8584efd1021370c25011265bc
stes94 <stes94@ymail.com>
2011-06-01 21:12:09 +0000

    Added new blocks

---
+++ src/destal/entities/blocks/Sand.java
@@ -0,0 +1,14 @@
+package destal.entities.blocks;
+
+import destal.entities.Values;
+
+public class Sand extends Block
+{
+
+	@Override
+	public int getDataValue()
+	{
+		return Values.BLOCK_SAND;
+	}
+
+}
---
+++ src/destal/entities/blocks/Water.java
@@ -0,0 +1,13 @@
+package destal.entities.blocks;
+
+import destal.entities.Values;
+
+public class Water extends Block
+{
+
+	@Override
+	public int getDataValue()
+	{
+		return Values.BLOCK_WATER;
+	}
+}
