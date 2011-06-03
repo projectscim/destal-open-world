03afb4e101e06a88a2a613797f4346266953cd75
stes94@ymail.com <stes94@ymail.com@5dce331b-43ae-905c-cebe-8f68a0ec51fa>
2011-06-03 22:34:43 +0000

    bugfixes; successfully integrated multiplayer mode (up to 4 clients right now; TODO: eventhandling in Client when new client connects); TODO: optimization

--- trunk/src/destal/entities/characters/Player.java
+++ trunk/src/destal/entities/characters/Player.java
@@ -4,6 +4,16 @@ import destal.entities.Values;
 
 public class Player extends Entity
 {
+	private int _id;
+	
+	public void setID(int id)
+	{
+		_id = id;
+	}
+	public int getID()
+	{
+		return _id;
+	}
 	
 	@Override
 	public int getDataValue()
