f802ce7219070757d0c1ca4611f74f6431cadbaf
stes94 <stes94@ymail.com>
2011-06-08 12:45:13 +0000

    Started documentation

--- src/destal/general/Server.java
+++ src/destal/general/Server.java
@@ -3,12 +3,20 @@ package destal.general;
 import destal.general.net.server.NetworkServer;
 import destal.general.ui.ServerGUI;
 
+/**
+ * The main class for the destal server application
+ * @author Alex Belke, Dennis Sternberg, Steffen Schneider
+ */
 public class Server
 {
 	private Controller _controller;
 	private NetworkServer _networkServer;
 	private ServerGUI _serverGui;
 	
+	/**
+	 * Creates a new server
+	 * @param gui true to enable the server GUI
+	 */
 	public Server(boolean gui)
 	{
 		_controller = new Controller();
@@ -19,16 +27,27 @@ public class Server
 		}
 	}
 	
+	/**
+	 * Returns the server GUI
+	 * @return The server GUI
+	 */
 	public ServerGUI getServerGUI()
 	{
 		return _serverGui;
 	}
 	
+	/**
+	 * Returns the network controller of this server
+	 * @return The network controller of this server
+	 */
 	public Controller getController()
 	{
 		return _controller;
 	}
 	
+	/**
+	 * Runs the server
+	 */
 	public void run()
 	{
 		(new Thread(_networkServer)).start();
